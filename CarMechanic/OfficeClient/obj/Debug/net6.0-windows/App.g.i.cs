34b4bd7ec52b879242bea9a71fbd8c0ad85cbbf1
SzuperKrisztus <xxcreyzzyxx@gmail.com>
2022-06-01 19:32:47 +0200

    Foltozgatas

--- CarMechanic/OfficeClient/obj/Debug/net6.0-windows/App.g.i.cs
+++ CarMechanic/OfficeClient/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace OfficeClient {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace OfficeClient {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public static void Main() {
             OfficeClient.App app = new OfficeClient.App();
             app.InitializeComponent();
---
+++ CarMechanic/Server/Controllers/CardataModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarMechanicLibrary.Models;
+
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardataModel : ControllerBase
+    {
+        // GET: api/<CardataModel>
+        [HttpGet]
+        public IEnumerable<Cardata> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CardataModel>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CardataModel>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CardataModel>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CardataModel>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

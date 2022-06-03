fca5521fc711c3585e8939ed51ea8c876065dda8
la7ci9 <akarmiis997@gmail.com>
2022-06-03 15:43:34 +0200

    OfficeClient done, just need to test

--- CarMechanic/OfficeClient/MainWindow.xaml.cs
+++ CarMechanic/OfficeClient/MainWindow.xaml.cs
@@ -22,6 +22,21 @@ namespace OfficeClient
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs args)
         {
             var selectedCarData = WorkListView.SelectedItems as Cardata;
+
+            if (selectedCarData != null)
+            {
+                _cardata = selectedCarData;
+                CustomerName.Text = selectedCarData.Name;
+                CarName.Text = selectedCarData.CarName;
+                CarType.Text = selectedCarData.CarType;
+                PlateNumber.Text = selectedCarData.PlateNumber;
+                ProblemDescription.Text = selectedCarData.ProblemDescip;
+
+                AddButton.Visibility = Visibility.Collapsed;
+                EditButton.Visibility = Visibility.Visible;
+                DeleteButton.Visibility = Visibility.Visible;
+
+            }
         }
 
         private void AddWork_Click(object sender, RoutedEventArgs e)
@@ -33,7 +48,6 @@ namespace OfficeClient
         
             if (ValidateFields.FieldsNotEmpty(CustomerName.Text, CarName.Text, CarType.Text, PlateNumber.Text, ProblemDescription.Text))
             {
-                MessageBox.Show("Yeeeee");
                 _cardata.Name = CustomerName.Text;
                 _cardata.CarName = CarName.Text;
                 _cardata.CarType = CarType.Text;
@@ -41,25 +55,45 @@ namespace OfficeClient
                 _cardata.Status = (WebApi_Common.Models.Statuses)Statuses.Recorded;
                 _cardata.ProblemDescip = ProblemDescription.Text;
                 _cardata.IntakeDate = DateTime.Now;
+
                 //Küldeni a szervernek
+                DataProvider.CreateCardata(_cardata);
+                UpdateWorkListView();
             }
         }
 
         private void EditWork_Click(object sender, RoutedEventArgs e)
         {
 
+            if (ValidateFields.FieldsNotEmpty(CustomerName.Text, CarName.Text, CarType.Text, PlateNumber.Text, ProblemDescription.Text))
+            {
+                _cardata.Name = CustomerName.Text;
+                _cardata.CarName = CarName.Text;
+                _cardata.CarType = CarType.Text;
+                _cardata.PlateNumber = PlateNumber.Text;
+                _cardata.Status = (WebApi_Common.Models.Statuses)Statuses.Recorded;
+                _cardata.ProblemDescip = ProblemDescription.Text;
+                _cardata.IntakeDate = DateTime.Now;
+
+                DataProvider.UpdateCardata(_cardata);
+                UpdateWorkListView();
+            }
         }
 
         private void DeleteWork_Click(object sender, RoutedEventArgs e)
         {
-
+            if (MessageBox.Show("Do you want to delete?", "Question", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                DataProvider.DeleteCardata(_cardata.Id);
+                UpdateWorkListView();
+            }
         }
 
         private void UpdateWorkListView()
         {
+            //frissíti a listát
             var cardatas = DataProvider.GetCardata().ToList();
             WorkListView.ItemsSource = cardatas;
-            //frissíti a listát
         }
         public enum Statuses
         {

83dc4c4e9db02f08ec8d8eaa9836c460d6004ec4
la7ci9 <akarmiis997@gmail.com>
2022-06-03 14:53:42 +0200

    DataProvider class and repairs in MainWindow

---
+++ CarMechanic/OfficeClient/CardataProvider/DataProvider.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using WebApi_Common.Models;
+
+namespace OfficeClient.CardataProvider
+{
+    class DataProvider
+    {
+
+        private const string _url = "http://localhost:5000/api/cardata";
+
+        public static IEnumerable<Cardata> GetCardata()
+        {
+            using(var client = new HttpClient())
+            {
+                var response = client.GetAsync(_url).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var rawData = response.Content.ReadAsStringAsync().Result;
+                    var cardatas = JsonConvert.DeserializeObject<IEnumerable<Cardata>>(rawData);
+                    return cardatas;
+                }
+                throw new InvalidOperationException(response.StatusCode.ToString());
+            }
+        }
+
+        public static void CreateCardata(Cardata cardata)
+        {
+            using (var client = new HttpClient())
+            {
+                var rawData = JsonConvert.SerializeObject(cardata);
+                var content = new StringContent(rawData, Encoding.UTF8, "application/json");
+
+                var response = client.PostAsync(_url, content).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException(response.StatusCode.ToString());
+                }
+            }
+        }
+
+        public static void UpdateCardata(Cardata cardata)
+        {
+            using (var client = new HttpClient())
+            {
+                var rawData = JsonConvert.SerializeObject(cardata);
+                var content = new StringContent(rawData, Encoding.UTF8, "application/json");
+
+                var response = client.PutAsync(_url, content).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException(response.StatusCode.ToString());
+                }
+            }
+        }
+
+        public static void DeleteCardata(long id)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.DeleteAsync($"{_url}/{id}").Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException(response.StatusCode.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ CarMechanic/OfficeClient/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using OfficeClient.CardataProvider;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using WebApi_Common.Models;
+
+namespace OfficeClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Cardata _cardata;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs args)
+        {
+            var selectedCarData = WorkListView.SelectedItems as Cardata;
+        }
+
+        private void AddWork_Click(object sender, RoutedEventArgs e)
+        {
+                _cardata = new Cardata();
+                AddButton.Visibility = Visibility.Visible;
+                EditButton.Visibility = Visibility.Collapsed;
+                DeleteButton.Visibility = Visibility.Collapsed;
+        
+            if (ValidateFields.FieldsNotEmpty(CustomerName.Text, CarName.Text, CarType.Text, PlateNumber.Text, ProblemDescription.Text))
+            {
+                MessageBox.Show("Yeeeee");
+                _cardata.Name = CustomerName.Text;
+                _cardata.CarName = CarName.Text;
+                _cardata.CarType = CarType.Text;
+                _cardata.PlateNumber = PlateNumber.Text;
+                _cardata.Status = (WebApi_Common.Models.Statuses)Statuses.Recorded;
+                _cardata.ProblemDescip = ProblemDescription.Text;
+                _cardata.IntakeDate = DateTime.Now;
+                //Küldeni a szervernek
+            }
+        }
+
+        private void EditWork_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteWork_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UpdateWorkListView()
+        {
+            var cardatas = DataProvider.GetCardata().ToList();
+            WorkListView.ItemsSource = cardatas;
+            //frissíti a listát
+        }
+        public enum Statuses
+        {
+            Recorded
+        }
+    }
+}
